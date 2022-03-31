d2391eebe4740fda15bfa66d05ac85e84aad5d32
vitor-ramalho <39497088+vitor-ramalho@users.noreply.github.com>
2022-03-30 21:53:20 -0300

    criacao Modal

--- src/Components/Feed/FeedModal.js
+++ src/Components/Feed/FeedModal.js
@@ -1,18 +1,27 @@
 import React from 'react';
 import styles from './FeedModal.module.css';
 import useFetch from '../../Hooks/useFetch'
+import Error from '../Helper/Error';
+import Loading from '../Helper/Loading';
 import { PHOTO_GET } from '../../api';
+import PhotoContent from '../Photo/PhotoContent';
 
-const FeedModal = ({photo}) => {
-  const {data, error, loading, request} = useFetch();
+const FeedModal = ({ photo, setModalPhoto }) => {
+  const { data, error, loading, request } = useFetch();
+
+  function handleOutsideClick(event) {
+    if (event.target === event.currentTarget) setModalPhoto(null)
+  }
 
   React.useEffect(() => {
-    const {url, options} = PHOTO_GET(photo.id)
+    const { url, options } = PHOTO_GET(photo.id)
     request(url, options)
   }, [photo, request])
   return (
-    <div className={styles.modal}>
-      <img stc={photo.src} alt="" />
+    <div className={styles.modal} onClick={handleOutsideClick}>
+      {error && <Error error={error} />}
+      {loading && <Loading />}
+      {data && <PhotoContent data={data} />}
     </div>
   )
 }
---
+++ src/Components/Photo/PhotoComments.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PhotoComments = () => {
+  return (
+    <div></div>
+  )
+}
+
+export default PhotoComments
